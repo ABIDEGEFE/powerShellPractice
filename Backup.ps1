0158156076fb5592670fd44ef2f743066fb0a216
Abinet <agonaferdegefe@gmail.com>
2025-11-15 19:04:54 +0300

    error handling using try/catch and decide whetere the error would be terminating or non-terminating

--- Backup.ps1
+++ Backup.ps1
@@ -1,23 +1,46 @@
 param(
-    [string]$path = "./WebApsdfp",
-    [string]$Destination = "./"
+    [string]$path,
+    [string]$destination,
+    [switch]$PathISWebApp
 )
 
-if( -not (test-path $path) ) {
-    throw "The specified path $path does not exist."
-    exit
-}
+# how can I make this script to terminate if any error occurs?
+# $ErrorActionPreference = "Stop"
 
-$date = Get-Date -Format "yyyyMMdd"
-$destinationFile = "$Destination/WebAppBackup_$date.zip"
+Import-Module DoesNotExist -ErrorAction stop # terminate execution if module not found
+Write-Host "This will not run"
 
-if (-not (test-path $destinationFile)) {
-    New-Item -ItemType File -Path $destinationFile -Force | Out-Null
+try{
+if ($pathIsWebApp){
+    $extension = "$((get-childitem $path).Extension | sort-object -unique)" -match "\.html | \.js | \.css"
+    if ($extension){
+        Write-host "your script looks like a web application."
+    }else{
+        throw "The specified path does not appear to be a web application."
+    }
 }
 else{
-    throw "Backup file $destinationFile already exists."
-    exit
+    # if I want to do something with a file instead of web app, the switch paramater help me to decide that.
+    new-item -path $path -itemtype file -name "testfile.txt" -force | out-null
+    exit 0
+}
+}
+catch{
+throw "An error occurred while validating the web application path: $($_.Exception)"
 }
 
-Compress-Archive -path $path -DestinationPath "$destinationFile" -Force
-Write-Host "Backup of $path completed at $destinationFile"
+
+if ( -not (test-path $destination)){
+    write-error "The specified destination $destination does not exist. $($_.exception.messag)"
+}   
+
+$date = Get-Date -Format "yyyyMMdd"
+$destinationPath = join-path -path $destination -childpath "Backup_$date"
+
+try{
+   Compress-Archive -path $path -DestinationPath $destinationPath
+}catch{
+    write-error "An error occurred while creating the backup archive: $_.exception.message"
+}
+
+write-host "Backup completed successfully. Archive created at $destinationPath"
